6fa618c749350a72f66ee67f1beca7247dc23000
moiseshxs <moises.santos@sptech.school>
2025-05-30 17:49:20 -0300

    Removendo arquivos de java

--- back-end/LogJava/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import controller.AppController;
-
-public class Main {
-    public static void main(String[] args) {
-        AppController app = new AppController();
-        app.iniciar();
-    }
-}
